26c6aa55b8c15d93843417d3697d6b2086c688fe
Tim Kirves <tim.kirves@gmail.com>
2020-04-14 23:34:53 +0300

    initial commit new testing repo via git

---
+++ src/main/java/Day6.java
@@ -0,0 +1,11 @@
+public class Day6 {
+
+
+    public static void printingResult(){
+        System.out.println("Hello world");
+    }
+
+    public static void main(String[] args) {
+        printingResult();
+    }
+}

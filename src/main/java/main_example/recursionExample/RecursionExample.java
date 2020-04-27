6c8af9a1f6cf9e38f099830004d17a872a47e9b9
Tim Kirves <tim.kirves@gmail.com>
2020-04-27 04:55:43 +0300

    recursionExample

--- src/main/java/main_example/recursionExample/RecursionExample.java
+++ src/main/java/main_example/recursionExample/RecursionExample.java
@@ -2,7 +2,29 @@ package main_example.recursionExample;
 
 public class RecursionExample {
 
-    public static void main(String[] args) {
+    public static void reduceByOne(int n) {
+        if (n >= 0) {
+            reduceByOne(n - 1);
+        }
+        System.out.println("Completed call: " + n);
+    }
 
+
+    public static void printNumbers(int num) {
+        if (num == 0) {
+            return;
+        } else {
+            System.out.println(num);
+            printNumbers(num - 1);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        reduceByOne(10);
+        System.out.println(" ");
+        printNumbers(10);
     }
 }
+
+
